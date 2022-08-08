1532994a8569f596928a1cdb1942d759c5842172
hh <zehraakan1998@gmail.com>
2022-08-08 18:09:25 +0300

    Initial commit

---
+++ src/day01/Primitives.java
@@ -0,0 +1,15 @@
+package day01;
+
+public class Primitives {
+
+    public static void main(String[] args) {
+
+        int a = 100;
+        double b = 2.5;
+        char c = 'C';
+        boolean d = true;
+
+        byte x = 100;
+        int y = x;
+    }
+}
